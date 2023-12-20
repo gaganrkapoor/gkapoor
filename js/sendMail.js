1c8bb050ef7b3e26144132394469a815cff40fbb
gagan kapoor <gagan.kapoor@tpgtelecom.com.au>
2023-12-20 14:17:36 +1100

    new alerts and emailing functionality working

--- js/sendMail.js
+++ js/sendMail.js
@@ -1,14 +1,27 @@
 function emailsend()
 {
+    var varuserName =document.getElementById('gkname').value;
+    var varemail = document.getElementById('gkemail').value;
+    var varsubject = document.getElementById('gksubject').value;
+    var varmessage = document.getElementById('gkmessage').value;
+    var varmessagebody = "Name:  " + varuserName +
+    "<br/> Email: " + varemail +
+    "<br/> Message: " + varmessage;
+
     Email.send({
         Host : "smtp.elasticemail.com",
         Username : "pumpkinhat.ltd@gmail.com",
         Password : "4FF269589ECDD729D79BFCF09A2B4571EEE3",
-        To : 'gaganrkapoor@gmail.com',
+        To : 'pumpkinhat.ltd@gmail.com',
         From : "pumpkinhat.ltd@gmail.com",
-        Subject : "This is the subject",
-        Body : "And this is the body"
+        Subject : varsubject,
+        Body : varmessagebody
     }).then(
-      message => alert(message)
+      message => {
+       if (message == 'OK') {
+        swal("Good job!", "Your message has been sent!", "success");
+       }
+       else {swal("Oops!", "There was an internal error, please try again later !", "error");}
+      }
     );
 }
